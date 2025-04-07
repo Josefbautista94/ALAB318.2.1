43d11548060a24ca22bb380b89e973c770752f96
Jose <josefbautista94@gmail.com>
2025-04-06 22:00:13 -0400

    Completed part 3

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use(express.urlencoded({ extended: true }));
 // middleware that parses URL-encoded form data from POST requests
 // without this, req.body would be undefined
 
+app.use(express.static("public")) // Any file in /public can be accessed by its filename directly in the browser
+
 // creating my own middleware for practice (Logger)
 app.use((req, res, next) => {
     // app.use registers the middleware — it runs on every request to the server
@@ -47,6 +49,22 @@ app.get('/user/:name', (req, res) => {
     res.render('user', { name: userName });
     // Renders views/user.ejs and passes in { name: 'Jose' }
 });
+
+app.get("/download", (req, res) => { 
+    // When someone visits this endpoint, this function runs
+  
+    res.download(__dirname + '/public/Megaman_retro_3D_by_cezkid.webp', '8bitMegaMan.webp'); 
+    
+    // res.download is a built-in Express method that tells the browser:
+    // “Instead of displaying this file, download it.”
+  
+    // __dirname is a global Node variable that gives the full path to the current directory.
+    // Adding + '/public/Megaman_retro_3D_by_cezkid.webp' points to the exact file inside your public/ folder.
+  
+    // '8bitMegaMan.webp' is the name the file will be saved as when downloaded,
+    // instead of using the original filename.
+  });
+  
 
 app.post('/submit', (req, res) => {
     // POST route that receives form data
